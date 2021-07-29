5d975e5a4905d02caed00cf11796107ca72f0ea8
Michael <michaelleojacob@gmail.com>
2021-07-29 15:06:51 -0700

    table and input area is finished. Holy smokes, this was bonkers. The styling was off, refactoring the code, making things repeat less, the slider/checkbox. wowza. I'm glad this bit is done though and I can move on. Woot woot

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import './index.css';
 import header from './header/header';
+import input from './inputArea/input';
 
 (function myApp() {
 	const app = {
 		init: function () {
 			this.cacheDom();
 			this.runHeader();
+			this.renderInput();
 		},
 		cacheDom: function () {
 			this.parent = document.querySelector('#main');
 		},
 		runHeader: function () {
 			header(app.parent);
+		},
+		renderInput: function () {
+			input(app.parent);
 		},
 	};
 	app.init();
---
+++ src/inputArea/input.js
@@ -0,0 +1,66 @@
+import './input.css';
+
+export default function myInput(parent) {
+	(function () {
+		const input = {
+			init: function () {
+				this.makeInputWrapper();
+			},
+			makeInputWrapper: function () {
+				this.inwrap = document.createElement('div');
+				this.inwrap.classList.add('inwrap');
+				parent.appendChild(this.inwrap);
+				this.renderTable();
+			},
+			renderTable: function () {
+				this.makeTable();
+				this.makeTableRow('firstrow', 'Title:', 'firstrInput');
+				this.makeTableRow('secondrow', 'Author:', 'secondrInput');
+				this.makeTableRow('thirdrow', 'Pages:', 'thirdrInput');
+				this.makeCompletedRow();
+			},
+			makeTable: function () {
+				this.table = document.createElement('table');
+				this.table.classList.add('table');
+				this.table.setAttribute('style', 'width:100%');
+				this.inwrap.appendChild(this.table);
+			},
+			makeTableRow: function (tableDataName, text, inputName) {
+				this.tablerow = document.createElement('tr');
+				this.makeTableDataCell(tableDataName, text);
+				this.makeTableInput(inputName);
+				this.table.appendChild(this.tablerow);
+			},
+			makeTableDataCell: function (tableDataName, text) {
+				this.tableDataName = document.createElement('td');
+				this.tableDataName.classList.add('tdata', tableDataName);
+				this.tableDataName.textContent = text;
+				this.tablerow.appendChild(this.tableDataName);
+			},
+			makeTableInput: function (inputName) {
+				this.inputName = document.createElement('input');
+				this.inputName.classList.add('rowinput', inputName);
+				this.inputName.classList.add('input');
+				this.tablerow.appendChild(this.inputName);
+			},
+			makeCompletedRow: function () {
+				this.completed = document.createElement('td');
+				this.completed.textContent = 'Completed';
+				this.table.appendChild(this.completed);
+				this.makeSlider();
+			},
+			makeSlider: function () {
+				this.sliderLabel = document.createElement('label');
+				this.sliderLabel.classList.add('switch');
+				this.sliderInput = document.createElement('input');
+				this.sliderInput.setAttribute('type', 'checkbox');
+				this.sliderSpan = document.createElement('span');
+				this.sliderSpan.classList.add('slider', 'round');
+				this.sliderLabel.appendChild(this.sliderInput);
+				this.sliderLabel.appendChild(this.sliderSpan);
+				this.completed.appendChild(this.sliderLabel);
+			},
+		};
+		input.init();
+	})();
+}
