ec498295d75f5fd5ef56c51e85bd5f2ebaed94da
Michael <michaelleojacob@gmail.com>
2021-07-30 13:11:18 -0700

    currently pondering how to break this class up, because adding the addbook to ui functionality would mean uiclass.js is ganna get HUGE. if we could figure out a way to make it into two parts, one for the static ui elements, and one for the dynamic user input elements, that would be most excellent

--- src/uiclass/uiclass.js
+++ src/uiclass/uiclass.js
@@ -15,28 +15,25 @@ export default class UI {
 		this.allTablePieces();
 	}
 	static allTablePieces() {
-		this.makeTablehead();
 		this.makeFirstTableRow();
-		this.makeTableHeaders('Title');
-		this.makeTableHeaders('Author');
-		this.makeTableHeaders('Pages');
-		this.makeTableHeaders('Completed');
-		this.makeTableHeaders('X');
-	}
-	static makeTablehead() {
-		this.tablehead = document.createElement('div');
-		this.tablehead.classList.add('uitablehead');
-		this.uiwrap.appendChild(this.tablehead);
+		this.bundleTableHeaders();
 	}
 	static makeFirstTableRow() {
 		this.firstTableRow = document.createElement('div');
 		this.firstTableRow.classList.add('uitableRow');
-		this.tablehead.appendChild(this.firstTableRow);
+		this.uiwrap.appendChild(this.firstTableRow);
 	}
 	static makeTableHeaders(elementName) {
 		this.elementName = document.createElement('div');
 		this.elementName.classList.add(elementName, 'uitableheader');
 		this.elementName.textContent = elementName;
 		this.firstTableRow.appendChild(this.elementName);
+	}
+	static bundleTableHeaders() {
+		this.makeTableHeaders('Title');
+		this.makeTableHeaders('Author');
+		this.makeTableHeaders('Pages');
+		this.makeTableHeaders('Completed');
+		this.makeTableHeaders('X');
 	}
 }
