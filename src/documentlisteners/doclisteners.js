ebb569c5b11559054ffa7f0cef66366ad95e47bd
Michael <michaelleojacob@gmail.com>
2021-07-31 13:03:57 -0700

    added a file for global event listeners. only activates after all dom content is loaded.

--- src/documentlisteners/doclisteners.js
+++ src/documentlisteners/doclisteners.js
@@ -1,15 +1,22 @@
 import dynamicUI from '../uiclass/dynamicui/dynamicui';
 
 export default function myDocListeners() {
-	(function () {
-		const listener = {
-			init: function () {
-				this.loadBooksFromStorage();
-			},
-			loadBooksFromStorage: function () {
-				dynamicUI.displayBooks();
-			},
-		};
-		listener.init();
-	})();
+	const listener = {
+		init: function () {
+			this.loadBooksFromStorage();
+			this.listenForClickDelete();
+		},
+		loadBooksFromStorage: function () {
+			dynamicUI.displayBooks();
+		},
+		handleDelete: function (event) {
+			if (event.target.className.includes('deletebtn')) {
+				console.log(event);
+			}
+		},
+		listenForClickDelete: function () {
+			document.addEventListener('click', this.handleDelete);
+		},
+	};
+	listener.init();
 }
